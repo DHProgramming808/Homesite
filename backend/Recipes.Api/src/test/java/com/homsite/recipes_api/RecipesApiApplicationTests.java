834d1a72525d729aef2547694c6956f0b1792950
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2026-02-02 18:07:21 -0500

    initial spring initializr extraction

---
+++ backend/Recipes.Api/src/test/java/com/homsite/recipes_api/RecipesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homsite.recipes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
