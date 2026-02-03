9dd30d0d0f15f73aecefcb99b7ccbd22ec657653
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2026-02-02 22:22:00 -0500

    Update RecipesControllerRestWrapperAntiPattern.java

--- backend/recipes-api/src/main/java/com/homesite/recipes_api/controller/restwrapper/RecipesControllerRestWrapperAntiPattern.java
+++ backend/recipes-api/src/main/java/com/homesite/recipes_api/controller/restwrapper/RecipesControllerRestWrapperAntiPattern.java
@@ -1,16 +1,21 @@
 package com.homesite.recipes_api.controller.restwrapper;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/recipes")
 public class RecipesControllerRestWrapperAntiPattern {
 
 
-  @GetMapping("/recipes")
+    @GetMapping("/recipes")
     public String getRecipes() {
-        return "List of recipes";
+        return "List of featured/top recipes";
     }
 
-
+    @GetMapping("/recipe/{id}")
+    public String getRecipeById(String id) {
+        return "Recipe details for ID: " + id;
+    }
 }
