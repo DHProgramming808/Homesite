01be8ae35a9e5f0cc32739b05d37a8e7521d39c4
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2026-02-03 01:08:18 -0500

    revert antipattern
    we are going straight graphql for the recipes app.
    
    Currently leaving in some rest endpoint just for debugging and health checks.

---
+++ backend/recipes-api/src/main/java/com/homesite/recipes_api/controller/RecipesController.java
@@ -0,0 +1,9 @@
+package com.homesite.recipes_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class RecipesController {
+
+
+
+}
--- backend/recipes-api/src/main/java/com/homesite/recipes_api/controller/restwrapper/RestHealthController.java
+++ backend/recipes-api/src/main/java/com/homesite/recipes_api/controller/restwrapper/RestHealthController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthControllerRestWrapperAntiPattern {
+public class RestHealthController { // TODO Deprecate this class once GraphQL is fully adopted
 
 
   @GetMapping("/rest-health")
