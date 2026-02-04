72bcd0ba1b0b3cb8e910e8fc802444bf5b13f1d5
TheSoundofaFullStomach <106417339+TheSoundofaFullStomach@users.noreply.github.com>
2026-02-04 05:51:42 -0500

    fill in recipes controller with base functionality

--- backend/recipes-api/src/main/java/com/homesite/recipes_api/security/SecurityConfig.java
+++ backend/recipes-api/src/main/java/com/homesite/recipes_api/security/SecurityConfig.java
@@ -1,13 +1,25 @@
 package com.homesite.recipes_api.security;
 
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.*;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
+  @Value("${security.jwt.secret}")
+  private String jwtSecret;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,9 +28,20 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
               .requestMatchers("/health").permitAll()
                 .anyRequest().permitAll()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                    .decoder(jwtDecoder())
+                )
             );
 
         return http.build();
     }
 
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+      var secretKey = new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+      return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
 }
